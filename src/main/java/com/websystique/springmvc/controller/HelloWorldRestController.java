e7c740f36ca120c0a54413ae6ce333c14a9e9977
kprakashreddy07 <prakashreddyk07@gmail.com>
2017-09-15 08:12:43 +0530

    testing

--- src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldRestController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +28,7 @@ public class HelloWorldRestController {
     //-------------------Retrieve All Users--------------------------------------------------------
      
     @RequestMapping(value = "/user", method = RequestMethod.GET)
+    @ExceptionHandler
     public ResponseEntity<List<User>> listAllUsers() {
         List<User> users = userService.findAllUsers();
         if(users.isEmpty()){
